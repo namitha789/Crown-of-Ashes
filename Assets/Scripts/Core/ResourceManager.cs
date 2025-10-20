82b52080b922439fdf901ada311b243c0756cdc3
abhinav17reja <abhinav17reja@gmail.com>
2025-10-19 17:45:23 -0700

    sprint_1_changes

--- Assets/Scripts/Core/ResourceManager.cs
+++ Assets/Scripts/Core/ResourceManager.cs
@@ -1,70 +1,70 @@
 using UnityEngine;
 
-   public class ResourceManager : MonoBehaviour
-   {
-       public static ResourceManager Instance { get; private set; }
-       
-       [SerializeField] private ResourceData _playerResources;
-       
-       private void Awake()
-       {
-           if (Instance != null && Instance != this)
-           {
-               Destroy(gameObject);
-               return;
-           }
-           
-           Instance = this;
-           
-           // Initialize starting resources
-           _playerResources = new ResourceData
-           {
-               Gold = 200,
-               Materials = 100,
-               Influence = 0
-           };
-       }
-       
-       private void Start()
-       {
-           // Broadcast initial resources
-           UpdateResourcesUI();
-       }
-       
-       public void AddResources(int gold, int materials, int influence)
-       {
-           _playerResources.Gold += gold;
-           _playerResources.Materials += materials;
-           _playerResources.Influence += influence;
-           
-           UpdateResourcesUI();
-       }
-       
-       public bool SpendResources(int gold, int materials, int influence)
-       {
-           if (_playerResources.Gold >= gold && 
-               _playerResources.Materials >= materials && 
-               _playerResources.Influence >= influence)
-           {
-               _playerResources.Gold -= gold;
-               _playerResources.Materials -= materials;
-               _playerResources.Influence -= influence;
-               
-               UpdateResourcesUI();
-               return true;
-           }
-           
-           return false;
-       }
-       
-       public ResourceData GetPlayerResources()
-       {
-           return _playerResources;
-       }
-       
-       private void UpdateResourcesUI()
-       {
-           EventManager.TriggerEvent("ResourcesUpdated", _playerResources);
-       }
-   }
-   
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager Instance { get; private set; }
+    
+    [Header("Starting Resources")]
+    [SerializeField] private int _startingGold = 200;
+    [SerializeField] private int _startingMaterials = 100;
+    [SerializeField] private int _startingInfluence = 0;
+    
+    private ResourceData _playerResources;
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Instance = this;
+        
+        _playerResources = new ResourceData(_startingGold, _startingMaterials, _startingInfluence);
+    }
+    
+    private void Start()
+    {
+        UpdateResourcesUI();
+    }
+    
+    public void AddResources(int gold, int materials, int influence)
+    {
+        _playerResources.Gold += gold;
+        _playerResources.Materials += materials;
+        _playerResources.Influence += influence;
+        
+        Debug.Log($"Resources added: Gold+{gold}, Materials+{materials}, Influence+{influence}");
+        UpdateResourcesUI();
+    }
+    
+    public bool SpendResources(int gold, int materials, int influence)
+    {
+        if (_playerResources.Gold >= gold && 
+            _playerResources.Materials >= materials && 
+            _playerResources.Influence >= influence)
+        {
+            _playerResources.Gold -= gold;
+            _playerResources.Materials -= materials;
+            _playerResources.Influence -= influence;
+            
+            Debug.Log($"Resources spent: Gold-{gold}, Materials-{materials}, Influence-{influence}");
+            UpdateResourcesUI();
+            return true;
+        }
+        
+        Debug.LogWarning("Not enough resources!");
+        return false;
+    }
+    
+    public ResourceData GetPlayerResources()
+    {
+        return _playerResources;
+    }
+    
+    private void UpdateResourcesUI()
+    {
+        EventManager.TriggerEvent("ResourcesUpdated", _playerResources);
+    }
+}
--- Assets/Scripts/Economy/BuildingPlacer.cs
+++ Assets/Scripts/Economy/BuildingPlacer.cs
@@ -1,182 +1,122 @@
 using UnityEngine;
 
-   public class BuildingPlacer : MonoBehaviour
-   {
-       [SerializeField] private LayerMask _groundLayer;
-       [SerializeField] private Material _validPlacementMaterial;
-       [SerializeField] private Material _invalidPlacementMaterial;
-       
-       private GameObject _currentBuilding;
-       private BuildingMenu.BuildingType _currentType;
-       private int _goldCost;
-       private int _materialsCost;
-       private bool _isPlacing;
-       private Camera _mainCamera;
-       private Renderer[] _buildingRenderers;
-       private Material[] _originalMaterials;
-       
-       private void Awake()
-       {
-           _mainCamera = Camera.main;
-       }
-       
-       private void Update()
-       {
-           if (_isPlacing && _currentBuilding != null)
-           {
-               UpdateBuildingPosition();
-               
-               if (Input.GetMouseButtonDown(0))
-               {
-                   TryPlaceBuilding();
-               }
-               else if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Escape))
-               {
-                   CancelPlacement();
-               }
-           }
-       }
-       
-       public void StartPlacingBuilding(GameObject buildingPrefab, BuildingMenu.BuildingType type, int goldCost, int materialsCost)
-       {
-           if (_isPlacing)
-           {
-               CancelPlacement();
-           }
-           
-           _currentBuilding = Instantiate(buildingPrefab);
-           _currentType = type;
-           _goldCost = goldCost;
-           _materialsCost = materialsCost;
-           _isPlacing = true;
-           
-           // Store renderers and original materials
-           _buildingRenderers = _currentBuilding.GetComponentsInChildren<Renderer>();
-           _originalMaterials = new Material[_buildingRenderers.Length];
-           
-           for (int i = 0; i < _buildingRenderers.Length; i++)
-           {
-               _originalMaterials[i] = _buildingRenderers[i].material;
-           }
-           
-           // Disable colliders during placement
-           foreach (Collider collider in _currentBuilding.GetComponentsInChildren<Collider>())
-           {
-               collider.enabled = false;
-           }
-       }
-       
-       private void UpdateBuildingPosition()
-       {
-           Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-           
-           if (Physics.Raycast(ray, out RaycastHit hit, 1000f, _groundLayer))
-           {
-               _currentBuilding.transform.position = hit.point;
-               
-               // Check if placement is valid
-               bool isValid = IsValidPlacement();
-               
-               // Update materials based on validity
-               Material placementMaterial = isValid ? _validPlacementMaterial : _invalidPlacementMaterial;
-               
-               foreach (Renderer renderer in _buildingRenderers)
-               {
-                   renderer.material = placementMaterial;
-               }
-           }
-       }
-       
-       private bool IsValidPlacement()
-       {
-           // Simple placement validation - no overlapping colliders
-           Collider[] colliders = _currentBuilding.GetComponentsInChildren<Collider>();
-           
-           foreach (Collider collider in colliders)
-           {
-               // Temporarily enable the collider for checking
-               collider.enabled = true;
-               
-               // Check for overlapping colliders
-               Collider[] overlappingColliders = Physics.OverlapBox(
-                   collider.bounds.center,
-                   collider.bounds.extents,
-                   _currentBuilding.transform.rotation
-               );
-               
-               // Disable it again
-               collider.enabled = false;
-               
-               // If we found any colliders (other than our own building colliders)
-               if (overlappingColliders.Length > colliders.Length)
-               {
-                   return false;
-               }
-           }
-           
-           return true;
-       }
-       
-       private void TryPlaceBuilding()
-       {
-           if (IsValidPlacement())
-           {
-               if (ResourceManager.Instance.SpendResources(_goldCost, _materialsCost, 0))
-               {
-                   // Enable colliders
-                   foreach (Collider collider in _currentBuilding.GetComponentsInChildren<Collider>())
-                   {
-                       collider.enabled = true;
-                   }
-                   
-                   // Restore original materials
-                   for (int i = 0; i < _buildingRenderers.Length; i++)
-                   {
-                       _buildingRenderers[i].material = _originalMaterials[i];
-                   }
-                   
-                   // Add appropriate building component
-                   AddBuildingComponent(_currentType);
-                   
-                   // Building placed successfully
-                   _currentBuilding = null;
-                   _isPlacing = false;
-               }
-               else
-               {
-                   Debug.Log("Not enough resources!");
-                   CancelPlacement();
-               }
-           }
-       }
-       
-       private void AddBuildingComponent(BuildingMenu.BuildingType type)
-       {
-           switch (type)
-           {
-               case BuildingMenu.BuildingType.CommandCenter:
-                   _currentBuilding.AddComponent<CommandCenter>();
-                   break;
-               case BuildingMenu.BuildingType.Barracks:
-                   _currentBuilding.AddComponent<Barracks>();
-                   break;
-               case BuildingMenu.BuildingType.ResourceCollector:
-                   _currentBuilding.AddComponent<ResourceCollector>();
-                   break;
-               case BuildingMenu.BuildingType.DefenseTower:
-                   _currentBuilding.AddComponent<DefenseTower>();
-                   break;
-           }
-       }
-       
-       private void CancelPlacement()
-       {
-           if (_currentBuilding != null)
-           {
-               Destroy(_currentBuilding);
-               _currentBuilding = null;
-           }
-           
-           _isPlacing = false;
-       }
-   }
+public class BuildingPlacer : MonoBehaviour
+{
+    [Header("Placement Settings")]
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private Material _validPlacementMaterial;
+    [SerializeField] private Material _invalidPlacementMaterial;
+    
+    private GameObject _currentBuildingPreview;
+    private bool _isPlacing;
+    private Camera _mainCamera;
+    private int _buildingCostGold;
+    private int _buildingCostMaterials;
+    
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+    
+    private void Update()
+    {
+        if (_isPlacing && _currentBuildingPreview != null)
+        {
+            UpdateBuildingPreview();
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                TryPlaceBuilding();
+            }
+            else if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Escape))
+            {
+                CancelPlacement();
+            }
+        }
+    }
+    
+    public void StartPlacingBuilding(GameObject buildingPrefab, int goldCost, int materialsCost)
+    {
+        if (_isPlacing)
+        {
+            CancelPlacement();
+        }
+        
+        _currentBuildingPreview = Instantiate(buildingPrefab);
+        _buildingCostGold = goldCost;
+        _buildingCostMaterials = materialsCost;
+        _isPlacing = true;
+        
+        // Disable colliders during preview
+        foreach (Collider col in _currentBuildingPreview.GetComponentsInChildren<Collider>())
+        {
+            col.enabled = false;
+        }
+        
+        Debug.Log($"Placing building: {buildingPrefab.name}");
+    }
+    
+    private void UpdateBuildingPreview()
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000f, _groundLayer))
+        {
+            _currentBuildingPreview.transform.position = hit.point;
+            
+            // Check if placement is valid
+            bool isValid = IsValidPlacement();
+            
+            // Update material based on validity
+            Renderer[] renderers = _currentBuildingPreview.GetComponentsInChildren<Renderer>();
+            foreach (Renderer renderer in renderers)
+            {
+                renderer.material = isValid ? _validPlacementMaterial : _invalidPlacementMaterial;
+            }
+        }
+    }
+    
+    private bool IsValidPlacement()
+    {
+        // Simple check - can be expanded
+        return true; // For now, always valid
+    }
+    
+    private void TryPlaceBuilding()
+    {
+        if (IsValidPlacement())
+        {
+            if (ResourceManager.Instance.SpendResources(_buildingCostGold, _buildingCostMaterials, 0))
+            {
+                // Enable colliders
+                foreach (Collider col in _currentBuildingPreview.GetComponentsInChildren<Collider>())
+                {
+                    col.enabled = true;
+                }
+                
+                // Restore original material (create a proper building material)
+                Renderer[] renderers = _currentBuildingPreview.GetComponentsInChildren<Renderer>();
+                foreach (Renderer renderer in renderers)
+                {
+                    // You'll need to assign proper materials here
+                    renderer.material.color = Color.white;
+                }
+                
+                Debug.Log("Building placed successfully!");
+                _currentBuildingPreview = null;
+                _isPlacing = false;
+            }
+        }
+    }
+    
+    private void CancelPlacement()
+    {
+        if (_currentBuildingPreview != null)
+        {
+            Destroy(_currentBuildingPreview);
+        }
+        
+        _isPlacing = false;
+        Debug.Log("Building placement cancelled");
+    }
+}
